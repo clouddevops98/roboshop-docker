482c86c8ef2ef076edecc3553e11225ccb518f80
somayya <somayya.adusumalli98@gmail.com>
2025-12-20 19:39:49 +0530

    k8s

--- mysql/custom-script.sh
+++ mysql/custom-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -f /tmp/mysql-root-password.txt]; then
+if [ -f /tmp/mysql-root-password.txt ]; then
     PASSWORD=$(cat /tmp/mysql-root-password.txt)
-    echo "Accessed MYSQL root password"
+    echo "Accessed MySQL root password"
 else
-   echo "MySQL Root password file not found"
-   exit 1
+    echo "MySQL Root password file not found"
+    exit 1
 fi
 
 export MYSQL_ROOT_PASSWORD=$PASSWORD
